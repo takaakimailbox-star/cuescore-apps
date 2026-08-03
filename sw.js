0c949ea4f8a3d02da41337d432445a06ddc8c083
takaakimailbox-star <takaaki.mailbox@gmail.com>
2026-08-03 09:48:06 +0900

    Compact player registration modal

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = "1.0-player-navigation-v4";
+const APP_VERSION = "1.0-player-editor-compact-v1";
 const CACHE_NAME = `cuescore-rotation-v${APP_VERSION}`;
 const APP_SHELL = [
   "./",
