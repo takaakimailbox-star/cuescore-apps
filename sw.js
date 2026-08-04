f6d9a1fc321287e49431f56f71bfc0abcc684d3b
takaakimailbox-star <takaaki.mailbox@gmail.com>
2026-08-04 21:29:27 +0900

    Add centralized feature access foundation

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = "1.0-break-foul-transfer-v1";
+const APP_VERSION = "1.0-feature-access-foundation-v1";
 const CACHE_NAME = `cuescore-rotation-v${APP_VERSION}`;
 const APP_SHELL = [
   "./",
