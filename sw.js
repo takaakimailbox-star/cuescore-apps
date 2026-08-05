dcc3986fe8438a4afd0857fe63672b6e74e4b4bb
takaakimailbox-star <takaaki.mailbox@gmail.com>
2026-08-05 22:20:02 +0900

    Refine match player and history layouts

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = "1.0-match-rules-avatar-v1";
+const APP_VERSION = "1.0-player-avatar-layout-v1";
 const CACHE_NAME = `cuescore-rotation-v${APP_VERSION}`;
 const APP_SHELL = [
   "./",
