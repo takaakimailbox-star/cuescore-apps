ac46d9bea112244b997d75b6109b11dbc9f160ef
takaakimailbox-star <takaaki.mailbox@gmail.com>
2026-08-06 11:57:22 +0900

    Fix custom rack race input

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = "1.0-individual-race-targets-v1";
+const APP_VERSION = "1.0-custom-race-input-v1";
 const CACHE_NAME = `cuescore-rotation-v${APP_VERSION}`;
 const APP_SHELL = [
   "./",
