2a6e70ddc50c590a9a9fa466612666adb3ea27fe
takaakimailbox-star <takaaki.mailbox@gmail.com>
2026-08-07 17:04:34 +0900

    Retire bottom navigation across CueScore

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = "1.0-settings-header-navigation-v1";
+const APP_VERSION = "1.0-bottom-navigation-retired-v1";
 const CACHE_NAME = `cuescore-apps-v${APP_VERSION}`;
 const APP_SHELL = [
   "./",
