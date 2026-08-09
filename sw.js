8c4bdfced304f4b07fb6bff0c1df4c8ffb9a92d3
takaakimailbox-star <takaaki.mailbox@gmail.com>
2026-08-09 09:10:27 +0900

    Rebuild player history match navigation

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = "1.0-player-history-detail-layer-v1";
+const APP_VERSION = "1.0-player-history-navigation-v4";
 const CACHE_NAME = `cuescore-apps-v${APP_VERSION}`;
 const APP_SHELL = [
   "./",
